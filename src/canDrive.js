f58d39b66e84361d76f920f9a682e1a8299941ae
Thiago Andrade <thiagoadd14@gmail.com>
2024-12-03 16:40:05 -0300

    Update canDrive.js

--- src/canDrive.js
+++ src/canDrive.js
@@ -1,6 +1,13 @@
 function canDrive(age) {
-  // escreva seu código aqui
+  if(age >= 18){
+    console.log(`"Você pode dirigir."`);
+    
+  } else {
+    console.log(`"Você não pode dirigir."`)
+  }
 }
+
+canDrive(18)
 
 
 module.exports = {
